0fe2e28870a02e1a5c5e9f99a4741232d5e33afd
akshay kashyap <akshaykashyap.2013@gmail.comgit config -- global user.name akashyap2013>
2021-10-26 00:07:59 +0530

    final commit

--- main.js
+++ main.js
@@ -9,7 +9,21 @@ window.onload = function exampleFunction() {
     toggle.addEventListener('click', function(event){
         collapse.classList.toggle('active');
         console.log(toggle)
-    })
+    });
+
+    // #site-main .recent-work-area .images-flex
+    var grid = document.querySelector('#site-main .recent-work-area .images-flex');
+
+    var msnry = new Masonry( grid, {
+        itemSelector: '.grid-item',
+        // columnWidth: 210,
+        gutter: 100
+    });
+
+    // // init with selector
+    // var msnry = new Masonry( '.grid', {
+    // // options...
+    // });
     
 }
 
